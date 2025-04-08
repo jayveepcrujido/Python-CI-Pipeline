de20bf231553cc03659a99937e82b503ab96b302
unknown <jvcrujido@gmail.com>
2025-04-08 21:07:33 +0800

    test_app update

--- test_app.py
+++ test_app.py
@@ -1,8 +1,17 @@
 import pytest
-from app import add, subtract
+from app import StringUtils
 
-def test_add():
-    assert add(2, 3) == 5
+utils = StringUtils()
 
-def test_subtract():
-    assert subtract(5, 3) == 2
+def test_palindromes():
+    assert utils.isPalindrome("Madam") == True
+    assert utils.isPalindrome("Radar") == True
+    assert utils.isPalindrome("A") == True
+    assert utils.isPalindrome("") == True
+
+def test_non_palindromes():
+    assert utils.isPalindrome("Hello") == False
+    assert utils.isPalindrome("Python") == False
+
+def test_none_input():
+    assert utils.isPalindrome(None) == False
