8d30750b66a6d0cc1cfb5e3ba8169d5fee69ec9e
unknown <jvcrujido@gmail.com>
2025-04-08 21:05:45 +0800

    app update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-def add(x, y):
-    return x + y
-
-def subtract(x, y):
-    return x - y
+class StringUtils:
+    def isPalindrome(self, input_str):
+        if input_str is None:
+            return False
+        reversed_str = input_str[::-1]
+        return input_str.lower() == reversed_str.lower()  
