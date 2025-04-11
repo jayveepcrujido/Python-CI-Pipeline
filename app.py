064f60e393b3b3baed0d85ddc55791ac6175f677
unknown <jvcrujido@gmail.com>
2025-04-11 09:39:56 +0800

    update app

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ class StringUtils:
         if input_str is None:
             return False
         reversed_str = input_str[::-1]
-        return input_str.lower() == reversed_str.lower()  
+        return input_str.lower() == reversed_str.lower()
